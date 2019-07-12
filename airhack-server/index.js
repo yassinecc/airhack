175f20778d284be23e438dbcad1e1d66fb900295
Yassine Chbani <yassine.chbani@gmail.com>
2019-07-12 22:28:13 +0200

    Add dummy routes

--- airhack-server/index.js
+++ airhack-server/index.js
@@ -4,4 +4,12 @@ const port = 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/api/hooks/health', (req, res) => {
+  res.send('health');
+});
+
+app.post('/api/hooks/incomingTasks', (req, res) => {
+  res.send('coucou');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
