32616807786ff39fbb6b600a20c327ac11de9536
Yassine Chbani <yassine.chbani@gmail.com>
2019-07-12 23:46:18 +0200

    Add geolib

--- airhack-server/index.js
+++ airhack-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { getDistance } = require('geolib');
 const port = 5000;
+const EARTH_RADIUS = 6378;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,7 +14,16 @@ app.get('/api/hooks/health', (req, res) => {
 });
 
 app.post('/api/hooks/incomingTasks', (req, res) => {
-  res.send('coucou');
+  const newBody = { ...req.body, tasks: assignTaskers(req.body.tasks) };
+  console.log(
+    getDistance(
+      { latitude: 48.882149, longitude: 2.305142 },
+      { latitude: 48.856815, longitude: 2.390498 }
+    )
+  );
+  return res.send(newBody);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const assignTaskers = tasks => tasks.map(el => ({ ...el, assignee_id: 1 }));
