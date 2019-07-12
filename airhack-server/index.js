707e189c855183d7f85eb6f3be608f29ac4a4b41
Yassine Chbani <yassine.chbani@gmail.com>
2019-07-12 23:01:23 +0200

    Add body parser and debugger

--- airhack-server/index.js
+++ airhack-server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 5000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/api/hooks/health', (req, res) => {
